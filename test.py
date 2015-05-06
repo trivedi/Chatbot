fe7c7a8f39b9c3d83dffe37dde979e348b00b26d
santo141 <santo141@umn.edu>
2015-05-06 03:14:26 -0500

    made it like nishad

--- test.py
+++ test.py
@@ -10,8 +10,19 @@ print("\n")
 tagged = nltk.pos_tag(tokens)
 print("Tags:")
 print(tagged[0:len(tokens)])
+sentence_tags = tagged[0:len(tokens)]
+'''
+for i in sentence_tags:
+    for j in i:
+        print(j + ',')
+'''
 print("\n")
-
+'''
+nltk_tagset = ['CC','CD','DT','EX','EX','FW','IN','JJ','JJR','JJS','LS','MD',
+               'NN','NNP','NNPS','NNS','PDT','POS','PRP','PRP$','RB','RBR',
+               'RBS','RP','SYM','TO','UH','VB','VBD','VBG','VBN','VBP','VBZ',
+               'WDT','WP','WP$','WRB']
+'''
 entities = nltk.chunk.ne_chunk(tagged)
 print("Entities:")
 print(entities)
